aad7d80840cea548615675986dc0abd40edad63f
navi142 <109787937+navi142@users.noreply.github.com>
2022-09-25 19:33:43 +0530

    Update ec-2.tf

--- ec-2.tf
+++ ec-2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
   access_key = "AKIA2YDUMTTANMPSZXAD"
-  secrete_key = "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"
+  secret_key = "8Lo6RrdIwm2dlri1bLHGRWAVJ4fK9yqISwHTo/Hv"
 }
 
 resource "aws_instance" "ec2" {
