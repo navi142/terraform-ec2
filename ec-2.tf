7a6ebfd284f3ab40bbc7387b16d8b64887a55772
navi142 <109787937+navi142@users.noreply.github.com>
2022-09-25 20:54:52 +0530

    Update ec-2.tf

--- ec-2.tf
+++ ec-2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "this" {
+resource "aws_instance" "ec3" {
   ami                       = "ami-026b57f3c383c2eec"
   instance_type             = "t2.micro"
 }
