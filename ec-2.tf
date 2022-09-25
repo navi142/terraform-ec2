47696183ec75d9604362fbd823db108b88062ae8
navi142 <109787937+navi142@users.noreply.github.com>
2022-09-25 19:40:38 +0530

    Update ec-2.tf

--- ec-2.tf
+++ ec-2.tf
@@ -1,14 +1,12 @@
 provider "aws" {
+  profile = "default"
   region = "us-east-1"
-  access_key = "AKIA2YDUMTTANMPSZXAD"
-  secret_key = "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"
-}
+ }
 
 resource "aws_instance" "ec2" {
   ami = "ami-0636eac5d73e0e5d7"
   instance_type = "t2.micro"
-  key_name = "my-key"
-  tag {
+   tag {
     name = "linux"
     }
 }
